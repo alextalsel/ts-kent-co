dbb82062c967418ee5e8f19befe935aa4ed6683c
Alex T <alexaatalaat289@gmail.com>
2019-06-19 17:55:02 +0100

    h

--- README.md
+++
@@ -1,1 +0,0 @@
-# ts-kent-co
---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const PREFIX = '!';
+
+const token = 'NTkwOTQ2NTQ3NjI2Mjc4OTMy.XQpn9Q.1TXrNgyae9-9TbHKPrX7WWi_2D8';
+
+bot.on('ready', () =>{
+    console.log('Your bot is online!')
+})
+
+bot.on('message', message =>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.reply('pong!');
+            break;
+        case 'agree':
+            message.reply('Welcome to the T.S Kent Discord Server!')
+            message.channel.bulkDelete(args[0]);
+            break;
+    }
+})
+
+bot.on('ready', () => {
+    console.log('I am ready!');
+  });
+
+
+
+bot.login(process.env.token);
